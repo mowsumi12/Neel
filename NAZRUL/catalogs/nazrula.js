c8c6fefad216ca031233d0b0daa72b6466d04fd3
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-07-15 18:50:49 +0600

    Update nazrula.js

--- NAZRUL/catalogs/nazrula.js
+++ NAZRUL/catalogs/nazrula.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
 console.clear();
 function startBot(message) {
     (message) ? logger(message, "starting") : "";
-  console.log(chalk.blue('• X4 NAZRUL X4 SYSTEM •'));
+  console.log(chalk.blue('• X10 NAZRUL X10 SYSTEM •'));
   logger.loader(`deploying app on port ${chalk.blueBright(PORT)}`);
   app.listen(logger.loader(`app deployed on port ${chalk.blueBright(PORT)}`));
   const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "nazrulb.js"], {
